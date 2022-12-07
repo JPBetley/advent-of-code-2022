3541d9603270df780b55fc96069c098d56a09ea3
Phil Betley <jpbetley@gmail.com>
2022-12-06 20:28:08 -0500

    day 6 part one

---
+++ day-6/one.rb
@@ -0,0 +1,4 @@
+letters = File.readlines('input.txt', chomp: true).first.chars
+groups = letters.map.with_index { |x, i| letters.slice(i, 4) }
+uniq_index = groups.find_index { |x| x == x.uniq }
+puts(uniq_index + 4)
